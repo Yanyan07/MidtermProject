f1ffd7977ca17c400d891496be951419a0bf70b3
Brandon Pope <pope.bl@gmail.com>
2021-09-27 15:02:00 -0600

    database update and test correction

--- JPAHelpingHand/src/main/java/com/skilldistillery/helpinghand/entities/ShoppingCartItem.java
+++ JPAHelpingHand/src/main/java/com/skilldistillery/helpinghand/entities/ShoppingCartItem.java
@@ -21,11 +21,7 @@ public class ShoppingCartItem {
 
 	@OneToOne
 	@JoinColumn(name = "inventory_item_id")
-<<<<<<< HEAD
-	private InventoryItem invetoryItem;
-=======
 	private InventoryItem inventoryItem;
->>>>>>> ecf6afa5fb5b4bcf80b82afbcdeae1eef0be0b6e
 
 	@ManyToOne
 	@JoinColumn(name = "cart_id")
@@ -44,19 +40,11 @@ public class ShoppingCartItem {
 	}
 
 	public InventoryItem getInvetoryItem() {
-<<<<<<< HEAD
-		return invetoryItem;
-	}
-
-	public void setInvetoryItem(InventoryItem invetoryItem) {
-		this.invetoryItem = invetoryItem;
-=======
 		return inventoryItem;
 	}
 
 	public void setInvetoryItem(InventoryItem invetoryItem) {
 		this.inventoryItem = invetoryItem;
->>>>>>> ecf6afa5fb5b4bcf80b82afbcdeae1eef0be0b6e
 	}
 
 	public Cart getCart() {
@@ -86,14 +74,7 @@ public class ShoppingCartItem {
 
 	@Override
 	public String toString() {
-<<<<<<< HEAD
-		return "ShoppingCartItem [id=" + id + ", invetoryItem=" + invetoryItem + ", cart=" + cart + "]";
+		return "ShoppingCartItem [id=" + id + ", inventoryItem=" + inventoryItem + ", cart=" + cart + "]";
 	}
 
 }
-=======
-		return "ShoppingCartItem [id=" + id + ", invetoryItem=" + inventoryItem + ", cart=" + cart + "]";
-	}
-
-}
->>>>>>> ecf6afa5fb5b4bcf80b82afbcdeae1eef0be0b6e
